46c491073120a0f21c106831d1d22e4c3d6f040f
yasukawa <LYasukawa@stanfordhealthcare.org>
2019-12-21 00:48:15 -0800

    Fix http: to https: include file

--- src/DisplayReward.php
+++ src/DisplayReward.php
@@ -466,7 +466,7 @@ function sendRewardEmail($pid, $gcToken, $emailAddress) {
         <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous"/>
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/select2/4.0.6-rc.0/css/select2.min.css"/>
 
-        <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js"></script>
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js"></script>
         <script src="https://cdnjs.cloudflare.com/ajax/libs/select2/4.0.6-rc.0/js/select2.min.js"></script>
     </header>
     <body>
